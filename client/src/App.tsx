50c03190f3a8c350b4510519ba2acee1be761b31
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-12-08 22:59:56 +0530

    fixing struct and adding step workflow init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { CreateWorkflow } from './components/CreateWorkflow'
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/create-workflow' element={<CreateWorkflow />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
