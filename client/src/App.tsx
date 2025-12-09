5c030f59a1ef58cce506e78220cf121d4418bb86
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-12-09 12:31:18 +0530

    added shadcn and ts types

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { CreateWorkflow } from './components/CreateWorkflow'
+import CreateWorkflow from './components/CreateWorkflow'
 
 function App() {
   return (
--- client/src/components/CreateWorkflow.tsx
+++ client/src/components/CreateWorkflow.tsx
@@ -1,22 +1,30 @@
 import { useState, useCallback } from 'react';
 import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, type NodeChange } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-const initialNodes = [
-    { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-    { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
-];
-const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
+interface NodeType{
+    data:{
+        type : 'action' | 'trigger',
+        kind : 'price-trigger'|'time-trigger'|'hyperliquid'|'bagpack'|'lighter'
+    },
+    id: string, position: { x: number, y: number }
+}
+interface Edge{
+    id: string;
+    source : string;
+    target : string;
+}
 
 export default function CreateWorkflow() {
-    const [edges, setEdges] = useState([]);
-    const [nodes, setNodes] = useState([]);
+    const [edges, setEdges] = useState<Edge[]>([]);
+    const [nodes, setNodes] = useState<NodeType[]>([]);
 
     const onNodesChange = useCallback(
-        (changes : NodeChange) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+        (changes : any) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
         [],
     );
     const onEdgesChange = useCallback(
-        (changes : NodeChange) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+        (changes : any) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
         [],
     );
     const onConnect = useCallback(
